c396fd86b76289a57ce46d79cdb89e2c15cfd5b3
longpod97 <175112945+longpod97@users.noreply.github.com>
2024-10-05 21:49:45 +0800

    completed restaurant page

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import { createElement } from "./elementFactory";
 
 function createHome () {
     const main = document.getElementById("content")
-    createElement("div", "Welcome to The Rustic Sandwich, where we bring fresh, locally-sourced ingredients to your plate with a modern twist on classic flavors. Whether you're here for a quick bite or a special occasion, our cozy atmosphere and delicious menu are sure to satisfy", "class", "home tab-clicked", main)
+    createElement("div", "Welcome to The Rustic Sandwich, where we bring fresh, locally-sourced ingredients to your plate with a modern twist on classic flavors. Whether you're here for a quick bite or a special occasion, our cozy atmosphere and delicious menu are sure to satisfy", "class", "home", main)
 }
 
 export {createHome}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "./styles.css";
+import { createAbout } from "./about";
+import { createHome } from "./home";
+import { createMenu } from "./menu";
+
+const container = document.getElementById("content");
+const about = document.getElementById("aboutBttn");
+const menu = document.getElementById("menuBttn");
+const home = document.getElementById("homeBttn")
+
+
+about.addEventListener("click", () => {
+    container.innerHTML = ""
+    menu.classList.remove("tab-clicked");
+    home.classList.remove("tab-clicked")
+    about.classList.add("tab-clicked")
+    createAbout();
+
+})
+
+menu.addEventListener("click", () => {
+    container.innerHTML = "";
+    home.classList.remove("tab-clicked");
+    about.classList.remove("tab-clicked")
+    menu.classList.add("tab-clicked");
+    createMenu();
+    
+
+})
+
+home.addEventListener("click", () => {
+    container.innerHTML = "";
+    menu.classList.remove("tab-clicked");
+    about.classList.remove("tab-clicked")
+    home.classList.add("tab-clicked");
+    createHome();
+    
+
+})
+
+createHome();
